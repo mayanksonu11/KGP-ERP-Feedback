d60551bdc3996c2fc89960d5a370909a2a8c7f6e
Mayank Sonu <36286429+mayanksonu11@users.noreply.github.com>
2021-04-16 17:24:10 +0530

    Create theory.js

---
+++ theory.js
@@ -0,0 +1,17 @@
+document.getElementsByName("rad_0_1")[2].checked = true
+document.getElementsByName("rad_0_2")[3].checked = true
+document.getElementsByName("rad_0_3")[2].checked = true
+document.getElementsByName("rad_0_4")[2].checked = true
+document.getElementsByName("rad_0_5")[3].checked = true
+document.getElementsByName("rad_0_6")[4].checked = true
+document.getElementsByName("rad_0_7")[2].checked = true
+document.getElementsByName("rad_0_8")[3].checked = true
+document.getElementsByName("rad_0_9")[4].checked = true
+document.getElementsByName("rad_0_10")[2].checked = true
+document.getElementById("rad_1_11").innerHTML = "Has a very good knowledge of the subject. Explains things very clearly."
+document.getElementById("rad_2_11").innerHTML = "Nil"
+document.getElementById("rad_1_12").innerHTML = "Well-designed curriculum and gave me detailed insight of the topic"
+document.getElementById("rad_2_12").innerHTML = "Nil"
+document.getElementsByName("rad_0_13")[2].checked = true
+document.getElementsByName("rad_0_14")[2].checked = true  
+document.getElementById("rad_0_15").innerHTML = "Nil"
