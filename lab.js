4f3a6c5ce6bfe8af5a066fba99b0aa8a25b8ad49
Mayank Sonu <36286429+mayanksonu11@users.noreply.github.com>
2021-04-16 17:28:28 +0530

    Create lab.js

---
+++ lab.js
@@ -0,0 +1,16 @@
+document.getElementsByName("srad_0_1")[2].checked = true
+document.getElementsByName("rad_0_1")[2].checked = true
+document.getElementsByName("rad_0_2")[3].checked = true
+document.getElementsByName("rad_0_3")[2].checked = true
+document.getElementsByName("rad_0_4")[2].checked = true
+document.getElementsByName("rad_0_5")[3].checked = true
+document.getElementsByName("rad_0_6")[4].checked = true
+document.getElementsByName("rad_0_7")[2].checked = true
+document.getElementsByName("rad_0_8")[3].checked = true
+document.getElementById("rad_1_9").innerHTML = "Has a very good knowledge of the subject. Explains things very clearly."
+document.getElementById("rad_2_9").innerHTML = "Nil"
+document.getElementsByName("rad_0_10")[4].checked = true
+document.getElementsByName("rad_0_11")[2].checked = true
+document.getElementsByName("rad_0_12")[2].checked = true
+document.getElementsByName("rad_0_13")[2].checked = true  
+document.getElementById("rad_0_14").innerHTML = "Nil"
